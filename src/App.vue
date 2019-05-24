2bc4b539791160a623af5c934c17adbcef737025
Andrey Aristov <andrey@lumix.mcm.pasteur.fr>
2019-05-24 16:16:58 +0200

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img width=50% alt="Cartoon logo" src="./assets/dividing-amoeba.jpg">
+    <Form/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
---
+++ src/components/Form.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <span> OD1 </span>
+        <input v-model="OD1" type='number' min=0 placeholder="Your first OD">
+        <span> OD2 </span>
+        <input v-model="OD2" type='number' min=0 placeholder="Your second OD">
+        <span> period </span>
+        <input v-model="period" type='number' min=0  placeholder="Your time frame">
+        <br>
+        <p>Doubling time: {{doublingTime}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+  },
+  data() { 
+      return{
+        OD1: '',   
+        OD2: '',
+        period: ''
+      }
+  },
+  computed : {
+      doublingTime() {           
+          let growthRate = Math.log(this.OD2 / this.OD1) / this.period
+          return Math.log(2) / growthRate
+          
+      }
+  }
+
+}
+</script>
